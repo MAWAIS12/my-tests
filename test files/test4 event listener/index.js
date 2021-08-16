e84738e65ba282f4a828ee003addab0c8cdf00f3
MAWAIS12 <68752704+MAWAIS12@users.noreply.github.com>
2021-08-16 23:42:44 +0500

    Add files via upload

---
+++ test files/test4 event listener/index.js
@@ -0,0 +1,17 @@
+
+document.getElementById("myBtn").addEventListener("click", myFunction);
+
+function myFunction() {
+  let data = [
+    {
+        player: "Jane",
+        score: 52
+    }, 
+    {
+        player: "Mark",
+        score: 41
+    }
+];
+	console.log(data);
+
+}
